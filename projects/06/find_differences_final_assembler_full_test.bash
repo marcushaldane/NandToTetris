b7d26050b55d00ef11068e583ff0294baf420e42
marcushaldane <mrhaldane2017@gmail.com>
2024-04-29 13:40:56 -0500

    Added bash testing script for final version of assembler.py

--- projects/06/find_differences_final_assembler_full_test.bash
+++ projects/06/find_differences_final_assembler_full_test.bash
@@ -1,3 +1,21 @@
 #!/bin/bash
-
-sort my.hack test.hack | uniq -u
+echo "================="
+declare -a filePaths=( "./add/" "./max/" "./rect/")
+declare -a fileNames=( "my_Add.hack" "Add.hack" "my_Max.hack" "Max.hack" "my_Rect.hack" "Rect.hack" )
+idx=0
+for filePath in "${filePaths[@]}" ; do 
+    echo "Checking the following files:"
+    my_assembler_output_file_name="${filePath}${fileNames[idx]}"
+    echo ${my_assembler_output_file_name}
+    test_hack_file_name="${filePath}${fileNames[((idx+1))]}"
+    echo ${test_hack_file_name}
+    echo "-----------------"
+    differences=$(sort $my_assembler_output_file_name $test_hack_file_name | uniq -u)
+    if [ -z $differences ] ; then 
+        echo "No Differences Found"
+    else
+        echo "Differences Found: $differences"
+    fi
+    echo "================="
+    ((idx+=2))
+done
